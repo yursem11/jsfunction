6c25d07b5d7759d71701776c38bd49589aae3275
Yurii <yursem@meta.ua>
2016-10-02 12:32:50 +0000

    add new files

---
+++ index.js
@@ -0,0 +1,9 @@
+function square_equation ();
+var a = Number(prompt(input number));
+var b = Number(prompt(input number));
+var c = Number(prompt(input number));
+var x = Number
+var d = b * b - 4 * a * c
+if (d>0) { = (-b / 2*a) + (Math.sqrt(d) / 2*a),
+=(-b / 2*a) - (Math.sqrt(d) / 2*a);}
+document.writeln(x)
