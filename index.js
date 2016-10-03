5c41a48f21d25178573ef4753d510166af5f7e71
Yurii <yursem@meta.ua>
2016-10-03 15:38:29 +0000

    add new files

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 var a = Number(prompt('input a'));
 var b = Number(prompt('input b'));
 var c = Number(prompt('input c'));
-var x1;
-var x2;
+/*var x1;
+var x2;*/
 var d = b * b - 4 * a * c;
-var result = 'немає кореню'
+var result = 'немає кореню';
 alert(d);
-/*var x1 = -b / ( 2 * a ) +  Math.sqrt(d)/ ( 2 * a );*/
-function square ()
+var x1 = -b / ( 2 * a ) +  Math.sqrt(d)/ ( 2 * a );
+var x2 = -b / ( 2 * a ) -  Math.sqrt(d)/ ( 2 * a );
+
+if (d>0) { alert(x1);
+alert(x2);
+};
+if (d=0) { alert(x1);
+};
+if (d<0) {alert(result);
+}
+
+/*function square ()
 {
-if (d > 0) {
+    if (d > 0) {
     x1 = -b / ( 2 * a ) + Math.sqrt(d) / ( 2 * a );
     x2 = -b / ( 2 * a ) - Math.sqrt(d) / ( 2 * a );
-    }
-alert('x1');
-alert('x2 = x2');
-    if (d < 0) {
-        x1 = x2;
-            }
-        alert('x1 = ' + x1);
-            if (d<0) 
-            {
-        result = 'немає кореню';
-                        }
-}
-        alert(result);
-     
+    };
+        if (d = 0)
+    {alert(x1);
+        };
+if (d<0) {result = 'рівняння немає кореня'};
+    alert(result);
+    }*/
 
 
 
